7cdec5b6026ef90bac029f2561ab85db4ea05976
m93n <mohsen.k9731@gmail.com>
2023-04-21 22:37:25 +0430

    add dynamice sku

--- store/templatetags/custome_tag.py
+++ store/templatetags/custome_tag.py
@@ -5,10 +5,6 @@ register = template.Library()
 @register.simple_tag
 def divide(first_value, second_value):
 
-    print(first_value)
-    print(second_value)
-
-
     divide_value = int((1 - int(first_value) / int(second_value)) * 100)
 
     return divide_value
