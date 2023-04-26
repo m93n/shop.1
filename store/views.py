952762f46e57dd4ca203522681855bbbb92fd529
m93n <mohsen.k9731@gmail.com>
2023-04-26 10:57:24 +0430

    add cart_id, add_cart and cart_detail views and make cart template items, dynamic

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from store import models
 
 def home(request, category_slug=None):
@@ -22,8 +23,48 @@ def product(request, category_slug, product_slug):
 
     return render(request, 'store\product.html', {'product':product})
 
-def cart(request):
-    return render(request, 'store\cart.html')
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = models.Product.objects.get(id=product_id)
+    try:
+        cart = models.Cart.objects.get(cart_id=_cart_id(request))
+    except models.Cart.DoesNotExist:
+        cart = cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+        cart.save()
+    
+    try:
+        cart_item = models.CartItem.objects.get(product=product, cart=cart)
+        if cart_item.quantity < cart_item.product.stock:
+            cart_item.quantity += 1
+            cart_item.save()
+    except models.CartItem.DoesNotExist:
+        cart_item = models.CartItem.objects.create(
+            product=product,
+            cart=cart,
+            quantity = 1,
+        )
+        cart.save()
+
+    return redirect('cart_detail')
+
+def cart_detail(request, total=0, counter=0, cart_items=None):
+    try:
+        cart = models.Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = models.CartItem.objects.filter(cart=cart, active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.sale_price * cart_item.quantity)
+            counter += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+
+    return render(request, 'store\cart.html', dict(cart_items=cart_items, total=total, counter=counter))
 
 def checkout(request):
     return render(request, 'store\checkout.html')
